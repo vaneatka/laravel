0a97a7ba74a8fc42ed84ba264b4e68e420f15087
Io <vaneatka@gmail.com>
2019-08-18 21:03:38 +0300

    argh1

--- resources/views/admin/subscribeMany.blade.php
+++ resources/views/admin/subscribeMany.blade.php
@@ -4,6 +4,7 @@
 <form action="{{route('admin.subscribeMany')}}" method="POST">
     <h3>Vrei sa incarci tabelul cu mai multe inscrieri?</h3>
     <button type="submit" class="btn btn-primary">Incarca Tabelul</button>
+    <a href="/public/admin/subscribers">Show All</a>
     @csrf
 </form>
 @endsection
